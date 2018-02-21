9de930355c3f123ba95ff256afb3b837542e3568
stacyporter <mr.stacy.porter@gmail.com>
2018-02-21 17:33:01 +1000

    Quiz

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,8 +1,19 @@
 def check_guess(guess,answer):
     global score
-    if guess == answer:
-        print('Correct answer')
-        score = score + 1
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print('Correct answer')
+            score = score + 3 - attempt
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input('Sorry wrong answer. Try again. ')
+            attempt = attempt + 1
+    if attempt == 3:
+        print('The correct answer is ' + answer)
+            
 
 score = 0
 print('Guess the animal!')
